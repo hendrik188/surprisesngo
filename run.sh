4ee6d331402e0cb1cc145b802659ed50e8bb0fbe
Hendrik Chandra <88263889+hendrik188@users.noreply.github.com>
2024-03-21 23:41:48 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-ce23a9ad-8cda-47aa-b5fe-c6b20c07403e/)
 
 
 echo "$curl_output" >> Update.tsx
